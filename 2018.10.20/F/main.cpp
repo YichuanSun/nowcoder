84205ab87a7674777e2feaacab66e46272eaebd6
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-20 20:49:10 +0800

    很难受

--- 2018.10.20/F/main.cpp
+++ 2018.10.20/F/main.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
+#define MAX 10000000
 using namespace std;
 vector<int> v;
+void vdel(int x)    {
+    for (int i=0;i<(int)v.size();i++)
+        if (v[i]==x)    {v[i]=MAX;return;}
+}
+void otrank(int x)  {
+    for (int i=0;i<(int)v.size();i++)
+        w.push_back(v[i]);
+    sort(w.begin(),w.end());
+    for (int i=0;i<(int)w.size();i++)   {
+        if (w[i]==x)    {
+            cout<<i+1<<endl;
+            return;
+        }
+    }
+}
+void xrank(int x)  {
+    for (int i=0;i<(int)w.size();i++)   {
+        if (v[i]==x)    {
+            cout<<i+1<<endl;
+            return;
+        }
+    }
+}
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -9,7 +33,11 @@ int main()  {
         cin>>op>>x;
         switch(x)   {
             case 1: v.push_back(x);
-            case 2: v.erase(v.find(x));
+            case 2: vdel(x);
+            case 3: otrank(x);
+            case 4: xrank(x);
+            case 5: xpre(x);
+            case 6: xnext(x);
         }
     }
     return 0;
